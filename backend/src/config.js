c09fffb0627e376bb2c321783c7433ea99c379a4
Shane Kwapis <shane.kwapis@gmail.com>
2022-05-15 12:25:12 -0500

    Update pre-sale start

--- backend/src/config.js
+++ backend/src/config.js
@@ -68,7 +68,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-06-01T12:00:00+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-05-21T12:00:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-05-14T12:00:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x3DF68870d3bFb4d1ADb7fa0d6E30b9338ccd3547"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
